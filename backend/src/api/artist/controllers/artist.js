7c0cd4bf16c32fa33f87372a7787df3a3d6f3336
Paloma Oliveira <paloma.oliveira@saucelabs.com>
2023-01-29 19:25:59 +0100

    Add backend

--- backend/src/api/artist/controllers/artist.js
+++ backend/src/api/artist/controllers/artist.js
@@ -1,9 +1,23 @@
-'use strict';
+'use strict'
 
 /**
  * artist controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
+// Default controller for the `artist` model
+// module.exports = createCoreController('api::artist.artist');
 
-module.exports = createCoreController('api::artist.artist');
+// custom controller for the `artist` model
+
+module.exports = createCoreController('api::artist.artist', ({ strapi }) => ({
+  async find(ctx) {
+    const populateList = ['artistphoto', 'events', 'localizations']
+    // Push any additional query params to the array
+    populateList.push(ctx.query.populate)
+    ctx.query.populate = populateList.join(',')
+
+    const content = await super.find(ctx)
+    return content
+  },
+}))
--- backend/src/api/event/controllers/event.js
+++ backend/src/api/event/controllers/event.js
@@ -1,9 +1,24 @@
-'use strict';
+'use strict'
 
 /**
  * event controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::event.event');
+// Default controller for the `event` model
+module.exports = createCoreController('api::event.event')
+
+// Custom controller for the `event` model
+
+module.exports = createCoreController('api::event.event', ({ strapi }) => ({
+  async find(ctx) {
+    const populateList = ['image', 'artists', 'localizations']
+    // Push any additional query params to the array
+    populateList.push(ctx.query.populate)
+    ctx.query.populate = populateList.join(',')
+
+    const content = await super.find(ctx)
+    return content
+  },
+}))
